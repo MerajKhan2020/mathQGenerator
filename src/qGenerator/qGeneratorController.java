9417500f9231b8f58f580c4939e8c3a1511aed53
Meraj Khan <ashekeenkhan@gmail.com>
2021-09-02 22:42:29 +1000

    Added while loop and a method

--- src/qGenerator/qGeneratorController.java
+++ src/qGenerator/qGeneratorController.java
@@ -1,12 +1,31 @@
 package qGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class qGeneratorController {
 
-    public mathQuestionGenerator {
-        int a = 0;
-        int b = 0;
-        return 
+    public int topNumber;
+    public int bottomNumber;
+    public int answer;
+    public List list = new ArrayList<>(9);
+
+    public List<Integer> mathQuestionGenerator (){
+        //List<Integer> list = new ArrayList<>(9);
+        /*number1 = (int) (Math.random()*100);
+        number2 = (int) (Math.random()*10);*/
+
+        int i=0;
+        while (i<list.size()) {
+            topNumber = (int) (Math.random() * 100);
+            bottomNumber = (int) (Math.random() * 10);
+            list.add(topNumber);
+            list.add(bottomNumber);
+            list.add(topNumber * bottomNumber);
+        }
+        return list;
     }
+
 
 
 }
